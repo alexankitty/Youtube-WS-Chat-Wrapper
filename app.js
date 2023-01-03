8ac2b2a5af7eef34f904c51b79d5fbe021770d41
Alexankitty <alexankitty@gmail.com>
2023-01-02 19:46:19 -0700

    Fixed offline chats. Made callbacks unique

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { WebSocketServer } from 'ws';
 import YoutubeChat from './youtubeChatBotModule.js';
 import FS from 'fs';
 import tmi from 'tmi.js';
+import { v4 as uuidv4 } from 'uuid';
 
 function parseMessage(msgArr) {
   let message = "";
@@ -36,9 +37,14 @@ const wss = new WebSocketServer({
 
 const ytc = new YoutubeChat("./oauth2.keys.json", "tokens");
 var tmiIdentity;
+var knownIds;
 try{
   let rawData = FS.readFileSync('./tmi.credentials.json');
   tmiIdentity = JSON.parse(rawData);
+  if(FS.existsSync('./channels.json')){
+    let channelsRaw = FS.readFileSync('')
+    knownIds = JSON.parse(channelsRaw);
+  }
 }
 catch(err){
   console.error(err);
@@ -50,7 +56,6 @@ const tmiClient = new tmi.Client({
 });
 
 tmiClient.connect();
-tmiClient
 
 function heartbeat() {
     this.isAlive = true;
@@ -70,69 +75,77 @@ wss.on('connection', function connection(ws, req) {
         }
     })
     ws.liveChat;
+    ws.uuid = uuidv4();
     ws.waiting = true;
-    setTimeout(() => {
-      ws.waiting = false;//wait 5 seconds before we actually send messages so that the old stuff isn't sent.
-    }, 5000);
     ws.isAlive = true;
+    ws.tmiMessage = new Object;
     ws.send("Connected");
     ws.on('pong', heartbeat);
     console.log(`connection received from: ${ws.ip}`)
-      ws.on('close', function close() {
+      ws.on('close', async function close() {
         console.log(`Connection closed: ${ws.ip}`)
         clearInterval(interval);
         ws.liveChat.stop();
-        tmiClient.channels[ws.tmiId] = null;
-        tmiClient.disconnect();
-        tmiClient.connect();
+        tmiClient.part(ws.twitchChannel)
+        tmiClient.off("message", ws.tmiMessage[ws.uuid]);
       });
       ws.on('message', async function message(data) {
         try{
         const params = JSON.parse(data);
-        const twitchChannel = params.forward;
-
+        ws.twitchChannel = params.forward;
         if(params.channelName) {
-          params.id = await ytc.getChannelId(params.channelName);
+          if(!knownIds[params.channelName]){
+            knownIds[params.channelName] = await ytc.getChannelId(params.channelName);
+            FS.writeSync('./channels.json', JSON.stringify(knownIds, null, 2));
+          }
+          params.id = knownIds[params.channelName]
         }
-        let channelName = params.id;
-        if(twitchChannel){
-          tmiClient.channels.push(twitchChannel)
-          ws.tmiId = tmiClient.channels.length; 
-          tmiClient.connect()
-          tmiClient.on("message", (channel, userstate, message, self) => {
+        if(ws.twitchChannel){
+          ws.tmiMessage[ws.uuid] = (channel, userstate, message, self) => {
             if(self) return;
             console.log("sending twitch message to youtube")
             console.log(channel, message)
-            if(channel == `#${twitchChannel.toLowerCase()}` && !message.startsWith("[YouTube]")) {
+            if(channel == `#${ws.twitchChannel.toLowerCase()}` && !message.startsWith("[YouTube]")) {
               
               if(userstate['message-type' == 'action']) {
                 message = `*${message}*`
               }
               message = `[Twitch] ${userstate['display-name']}: ${message}`
-              ytc.sendMessageToChannel(message, channelName)
+              ytc.sendMessage(message, channelName)
             }
-          })
+          }
+          tmiClient.join(ws.twitchChannel)
+          tmiClient.on("message", ws.tmiMessage[ws.uuid])
         }
-        if(params.id.length == 24 && channelName.startsWith("UC")){
+        if(params.id.length == 24 && params.id.startsWith("UC")){
             ws.liveChat = new LiveChat({channelId: params.id})
         }
         else{
             ws.liveChat = new LiveChat({liveId: params.id})
         }
-        const ok = await ws.liveChat.start()
-        console.log(ws.liveChat)
+        setTimeout(() => {
+          ws.waiting = false;//wait 5 seconds before we actually send messages so that the old stuff isn't sent.
+        }, 5000);
+        let ok;
+        try{
+          ok = await ws.liveChat.start()
+        }
+        catch(e){
+          ws.send("400");
+          ws.close();
+        }
         if (!ok) {
-            ws.send(`400`)
+          ws.send(`400`)
         }
         ws.liveChat.on("chat", (chatItem) => {
             if(ws.waiting) return;
 
             ws.send(JSON.stringify(chatItem))
-            if(twitchChannel){
+            if(ws.twitchChannel){
                 let msg = parseMessage(chatItem.message);
                 if(msg.startsWith('[Twitch]')) return;
                 let messageBuilder = `[YouTube] ${chatItem.author.name}: ${msg}`;
-                tmiClient.say(twitchChannel, messageBuilder);
+                tmiClient.say(ws.twitchChannel, messageBuilder);
             }
           })
         ws.liveChat.on("error", (err) => {
@@ -140,6 +153,7 @@ wss.on('connection', function connection(ws, req) {
         })
         }
         catch(e){
+            console.error(e)
             ws.send(`401`)
             console.error("Invalid data received from client", e)
         }
@@ -147,12 +161,11 @@ wss.on('connection', function connection(ws, req) {
   });
   
   const interval = setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
+  wss.clients.forEach(async function each(ws) {
     if (ws.isAlive === false) {
     ws.liveChat.stop();
-    tmiClient.channels[ws.tmiId] = null;
-    tmiClient.disconnect();
-    tmiClient.connect();
+    tmiClient.part(ws.twitchChannel)
+    tmiClient.off("message", ws.tmiMessage[ws.uuid]);
     ws.liveChat = null;
     return ws.terminate();
     }
@@ -160,3 +173,9 @@ wss.on('connection', function connection(ws, req) {
     ws.ping();
   });
 }, 30000);
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason.stack);
+  // application specific logging, throwing an error, or other logic here
+});
+
--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import YoutubeChat from './YoutubeChatBotModule.js'
 
 async function main(){
     let ytc = new YoutubeChat("./oauth2.keys.json", "./newTokens");
-    ytc.sendMessageToChannel("channel name test", "alexankitty")
+
 }
 
 main().catch(console.error)
